7e942ffd6d1d2118017639aee165c90a5e3d45af
pcsishun <dekearthsa@hotmail.com>
2025-03-24 13:51:58 +0700

    upadte

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ const Home = () => {
   }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-xl font-bold mb-4">Temperature Chart</h1>
+      <h1 className="text-xl font-bold mb-4">Dashboard HLR</h1>
       {loading ? (
         <p>Loading...</p>
       ) : (
